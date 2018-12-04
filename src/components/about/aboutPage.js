7acf65a972952ccc9728a307cff896763f6fb3ad
Raphael Karanja <rkaranja@cytonn.com>
2018-12-04 12:06:57 +0300

    create the about page

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Raphael Karanja on 04/12/2018.
+ */
+import React, { Component } from 'react';
+
+class About extends Component {
+    render(){
+        return(
+            <div>
+                <h1>About </h1>
+                <ul>
+                    <li>React</li>
+                    <li>React Router</li>
+                    <li>Flux</li>
+                    <li>Node</li>
+                    <li>Gulp</li>
+                    <li>Browserify</li>
+                    <li>Bootstrap</li>
+                </ul>
+            </div>
+        )
+    }
+};
+
+export default About;
