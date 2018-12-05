a1c93a51068cae42e083146741e5cd862a925929
Raphael Karanja <rkaranja@cytonn.com>
2018-12-05 12:40:31 +0300

    Add various route files

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,24 @@
 /**
  * Created by Raphael Karanja on 05/12/2018.
  */
+import React from 'react';
+import Router from 'react-router';
+
+//components
+import HomePage from '../components/homePage';
+import AuthorsPage from '../components/author/authorPage';
+import AboutPage from '../components/about/aboutPage';
+
+
+let DefaultRoute = Router.DefaultRoute;
+let Route = Router.Route;
+
+
+
+const routes = (
+    <Route name="home" path="/" handler={HomePage}>
+        <DefaultRoute handler={HomePage} />
+        <Route name="authors" handler={AuthorsPage}/>
+        <Route name="about"  handler={AboutPage}/>
+    </Route>
+);
