8c39003c05dbbeefcc2dbfe328c2cd1b85180aa0
Raphael Karanja <rkaranja@cytonn.com>
2018-12-10 15:51:52 +0300

    group layout file together

--- src/components/Layout/Main/index.js
+++ src/components/Layout/Main/index.js
@@ -2,7 +2,7 @@
  * Created by Raphael Karanja on 10/12/2018.
  */
 import React from 'react';
-import Routes from '../../routes/routes'
+import Routes from '../../../routes/routes'
 // The Main component renders one of the three provided
 const Main = () => (
     <main>
--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,8 @@
  * Created by Raphael Karanja on 05/12/2018.
  */
 import React from 'react';
-import Header from './Header/index';
-import Main from './Main'
+import Header from './Layout/Header/index';
+import Main from './Layout/Main'
 const App = (props) => {
     return (
         <div>
