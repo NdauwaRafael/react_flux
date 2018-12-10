59d304c09ce98615829aa71244729e0c49be1a7a
Raphael Karanja <rkaranja@cytonn.com>
2018-12-10 15:40:53 +0300

    import and pass routes to the main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by Raphael Karanja on 10/12/2018.
  */
+import React from 'react';
+import Routes from '../../routes/routes'
+// The Main component renders one of the three provided
+const Main = () => (
+    <main>
+        {Routes}
+    </main>
+);
+
+export default Main
