7f971292c212d8141b8872ed5aba9ed8194018ad
Raphael Karanja <rkaranja@cytonn.com>
2018-12-13 12:12:32 +0300

    create a second 404 page

---
+++ src/components/404/NotFound.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../../css/404_2.css'
+export default ()=>
+    <div className="scene">
+        <div className="text">404</div>
+        <div className="text">Page Not Found!</div>
+        <br><br>
+            <div className="text">DO NOT TOUCH</div>
+            <div className="sheep">
+    <span className="top">
+      <div className="body"> </div>
+      <div className="head">
+        <div className="eye one"> </div>
+        <div className="eye two"> </div>
+        <div className="ear one"> </div>
+        <div className="ear two"> </div>
+      </div>
+    </span>
+                <div className="legs">
+                    <div className="leg"> </div>
+                    <div className="leg"> </div>
+                    <div className="leg"> </div>
+                    <div className="leg"> </div>
+                </div>
+            </div>
+    </div>
