c39fc676b3f830f4a88e42f04f890ee460cc74c6
Raphael Karanja <rkaranja@cytonn.com>
2018-12-13 12:20:28 +0300

    make 404 the default not found page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,10 +7,9 @@ import React from 'react';
 //components
 // import IndexPage from '../index';
 import HomePage from '../components/Home/index';
-import App from '../components/app';
 import AuthorsPage from '../components/author/authorPage';
 import AboutPage from '../components/about/aboutPage';
-import NotFound from '../components/404/NotFound'
+import NotFound from '../components/404'
 import {Route, Switch} from 'react-router-dom';
 
 
