e98d33c2c7e922a1283e4cd616b439cb0f490669
Raphael Karanja <rkaranja@cytonn.com>
2018-12-13 16:01:36 +0300

    bind inputs with passed author prop

--- src/components/author/Manage/Add/index.js
+++ src/components/author/Manage/Add/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default () =>
+export default ({author}) =>
     <div className="page_body">
         <form>
             <div className="form-group">
                 <label htmlFor="authorName">First Name</label>
                 <input type="text"
+                       value={author.firstname}
                        className="form-control"
                        id="authorName"
                        placeholder="Author First Name"/>
@@ -14,6 +15,7 @@ export default () =>
             <div className="form-group">
                 <label htmlFor="authorName">Last Name</label>
                 <input type="text"
+                       value={author.lastname}
                        className="form-control"
                        id="authorName"
                        placeholder="Author Last Name"/>
@@ -22,6 +24,7 @@ export default () =>
             <div className="form-group">
                 <label htmlFor="authorEmail">Email</label>
                 <input type="email"
+                       value={author.email}
                        className="form-control"
                        id="authorEmail"
                        aria-describedby="emailHelp"
