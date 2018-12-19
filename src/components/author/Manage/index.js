eb87046c6718690b5a6d45a025a7c2783447ce6c
Raphael Karanja <rkaranja@cytonn.com>
2018-12-19 12:34:36 +0300

    initialize a function to save author

--- src/components/author/Manage/index.js
+++ src/components/author/Manage/index.js
@@ -2,26 +2,31 @@ import React, {Component} from 'react';
 import AddAuthorForm from './Add'
 
 export default class extends Component {
-constructor(prop){
-    super(prop);
-    this.state = {
-        author: {
-            id: '',
-            firstname: '',
-            lastname: '',
-            email: ''
-        }
-    };
-    this.handleChange = this.handleChange.bind(this);
-}
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            author: {
+                id: '',
+                firstname: '',
+                lastname: '',
+                email: ''
+            }
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-    handleChange(event){
+    handleChange(event) {
         let field = event.target.name;
         let value = event.target.value;
         let author = this.state.author;
         author[field] = value;
         this.setState({author});
     };
+
+    saveAuthor() {
+
+    };
+
     render() {
         const {author} = this.state;
         return (
