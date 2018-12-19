68514168073813e20955d7cdde66a93f506441bf
Raphael Karanja <rkaranja@cytonn.com>
2018-12-19 16:15:43 +0300

    initialize form validator function

--- src/components/author/Manage/index.js
+++ src/components/author/Manage/index.js
@@ -12,10 +12,12 @@ export default class extends Component {
                 lastname: '',
                 email: ''
             },
-            baseUrl: 'http://localhost:3004/authors'
+            baseUrl: 'http://localhost:3004/authors',
+            errors: {}
         };
         this.handleChange = this.handleChange.bind(this);
         this.saveAuthor = this.saveAuthor.bind(this);
+        this.authorIsValid = this.authorIsValid.bind(this);
     }
 
     handleChange(event) {
@@ -25,8 +27,17 @@ export default class extends Component {
         author[field] = value;
         this.setState({author});
     };
-
+    authorIsValid(){
+        let {author, errors} = this.state;
+        if(author.firstname!=='' && author.lastname !== '' && author.email !== ''){
+            return true
+        }
+            return false
+    };
     saveAuthor(event) {
+        if(!this.authorIsValid){
+            return;
+        }
         event.preventDefault();
         const {baseUrl, author} = this.state;
         let options = {
@@ -42,10 +53,10 @@ export default class extends Component {
                 let authr = response;
                 console.log(authr, response.json())
                 this.props.history.push('/authors');
-                toastr.success(`Author details were registered successfully.`, 'Author Added Successfully', {timeOut: 5000});
+                toastr.success(`Author details were registered successfully.`, {timeOut: 5000});
             })
             .catch((error) => {
-                toastr.error('Author Details could not be saved. An Error occurred.', 'Oops!', {timeOut: 5000});
+                toastr.error('Author Details could not be saved. An Error occurred.', {timeOut: 5000});
                 console.log('error', error)
             })
     };
