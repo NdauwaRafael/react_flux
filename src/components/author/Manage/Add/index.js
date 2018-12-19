582b06e72f5244dc4fdb469fd4435add6c4d9f1e
Raphael Karanja <rkaranja@cytonn.com>
2018-12-19 12:56:32 +0300

    bind save author method to save author button

--- src/components/author/Manage/Add/index.js
+++ src/components/author/Manage/Add/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import TextInput from '../../../Common/TextInput'
 
-export default ({author, onChange}) =>
+export default ({author, onChange, save}) =>
 
     <div className="page_body">
         <form>
             <TextInput name="firstname" label="First Name" onChange={onChange} value={author.firstname}/>
             <TextInput name="lastname" label="Email" onChange={onChange} value={author.lastname}/>
             <TextInput name="email" label="First Name" onChange={onChange} value={author.email}/>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" onClick={save} className="btn btn-primary">Submit</button>
         </form>
     </div>
