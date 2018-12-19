8b72dfcc3d54478dc6b31405a7441b13d83ea95a
Raphael Karanja <rkaranja@cytonn.com>
2018-12-19 12:19:00 +0300

    add required props

--- src/components/Common/TextInput/index.js
+++ src/components/Common/TextInput/index.js
@@ -1,30 +1,31 @@
 /**
  * Created by Raphael Karanja on 2018-12-19.
  */
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
- class TextInput extends Component {
-    constructor(props){
+
+class TextInput extends Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
+    render() {
         const {name, value, label, error, onChange} = props;
-        const validatorClass= ()=>{
-            if(error.legth > 0) {
+        const validatorClass = () => {
+            if (error.legth > 0) {
                 return 'is-invalid'
             } else {
                 return '';
             }
         };
-        return(
-            <div className="form-group " >
+        return (
+            <div className="form-group ">
                 <label htmlFor={name}>{label}</label>
                 <input type="text"
                        value={value}
                        name={name}
                        onChange={onChange}
-                       className={"form-control "+validatorClass}
+                       className={"form-control " + validatorClass}
                        ref={name}
                        placeholder={'Input ' + label}/>
                 <small className="form-text text-muted invalid-feedback">{error}</small>
@@ -35,10 +36,11 @@ import PropTypes from 'prop-types';
 }
 
 TextInput.propTypes = {
-    name: PropTypes.string,
+    name: PropTypes.string.isRequired,
     value: PropTypes.string,
-    label: PropTypes.string,
-    onChange: PropTypes.func
+    label: PropTypes.string.isRequired,
+    error: PropTypes.string,
+    onChange: PropTypes.func.isRequired
 
 };
 export default TextInput;
