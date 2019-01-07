3453933c1d808655bb0e6b670d749cfd5d07e06e
Raphael Karanja <rkaranja@cytonn.com>
2019-01-07 10:03:06 +0300

    add Change listener to store

--- src/stores/AuthorStores/index.js
+++ src/stores/AuthorStores/index.js
@@ -7,5 +7,8 @@ import {EventEmitter} from 'events';
 import assign from 'object-assign';
 
 const AuthorStore = assign({}, EventEmitter.prototype, {
-
+    addChangeListener: (callback)=>{
+        this.on('change', callback);
+    },
+    
 });
