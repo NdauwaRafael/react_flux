f5630a16413c3192de82734f3c4cdeb22556cbf7
Raphael Karanja <rkaranja@cytonn.com>
2019-01-07 09:45:04 +0300

    pass save author api to create author action

--- src/actions/AuthorActions/index.js
+++ src/actions/AuthorActions/index.js
@@ -3,32 +3,19 @@ import toastr from "toastr";
 /**
  * Created by Raphael Karanja on 2019-01-06.
  */
-const Dispatcher = require('../../dispatcher');
-const ActionTypes = require('../../constants/ActionTypes');
+import Dispatcher from '../../dispatcher';
+import ActionTypes from '../../constants/ActionTypes';
+import AuthorApis from '../../API'
 
 const AuthorActions = {
-        createAuthor: (author, baseUrl)=>{
+    createAuthor: (author, baseUrl) => {
+        let newAuthor = AuthorApis.saveAuthor(author, baseUrl);
 
-
-          // return fetch(baseUrl, options)
-          //           .then((response) => {
-          //                   let authr = response;
-          //                   console.log(authr, response.json())
-          //                   this.props.history.push('/authors');
-          //                   toastr.success(`Author details were registered successfully.`, {timeOut: 5000});
-          //                   this.setState({
-          //                           dirty: true
-          //                   });
-          //           })
-          //           .catch((error) => {
-          //                   toastr.error('Author Details could not be saved. An Error occurred.', {timeOut: 5000});
-          //           })
-
-                Dispatcher.dispatch({
-                        actioType: ActionTypes.CREATE_AUTHOR,
-                        data: ''
-                })
-        }
+        Dispatcher.dispatch({
+            actioType: ActionTypes.CREATE_AUTHOR,
+            data: newAuthor
+        })
+    }
 };
 
 export default AuthorActions;
