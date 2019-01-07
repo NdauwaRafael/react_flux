7ec54cf0db0cddf2bd9bd57711b52e7e76d5744b
Raphael Karanja <rkaranja@cytonn.com>
2019-01-07 10:07:45 +0300

    add magic method holder for change

--- src/stores/AuthorStores/index.js
+++ src/stores/AuthorStores/index.js
@@ -5,15 +5,16 @@ import Dispatcher from '../../dispatcher';
 import ActionTypes from '../../constants/ActionTypes';
 import {EventEmitter} from 'events';
 import assign from 'object-assign';
+const CHANGE_EVENT = 'change';
 
 const AuthorStore = assign({}, EventEmitter.prototype, {
     addChangeListener: (callback)=>{
-        this.on('change', callback);
+        this.on(CHANGE_EVENT, callback);
     },
     removeChangeListener: (callback)=>{
-        this.on('change', callback);
+        this.on(CHANGE_EVENT, callback);
     },
     emitChange: ()=>{
-        this.emit('change');
+        this.emit(CHANGE_EVENT);
     }
 });
