088a4b1873aab1e29240aee94836a947497b95f8
Raphael Karanja <rkaranja@cytonn.com>
2019-01-07 10:00:01 +0300

    add all store dependencies and create store object

--- src/stores/AuthorStores/index.js
+++ src/stores/AuthorStores/index.js
@@ -1,5 +1,11 @@
 /**
  * Created by Raphael Karanja on 2019-01-07.
  */
-import Dispatcher from '../../dispatcher'
-import ActionTypes from '../../constants/ActionTypes'
+import Dispatcher from '../../dispatcher';
+import ActionTypes from '../../constants/ActionTypes';
+import {EventEmitter} from 'events';
+import assign from 'object-assign';
+
+const AuthorStore = assign({}, EventEmitter.prototype, {
+
+});
