cfd7e146e6f93d168e4243c042dbfd24a7396e53
Raphael Karanja <rkaranja@cytonn.com>
2019-01-09 12:46:39 +0300

    UPDATE: Call initApp method from initializer action file in the app page

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import Header from './Layout/Header/index';
 import Main from './Layout/Main';
+import AppInitializer from '../Flux/actions/InitializeActions';
+AppInitializer.initApp();
 
 
 const App = (props) => {
