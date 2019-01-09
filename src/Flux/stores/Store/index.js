825e9ca983a3341b7f232fee7b38e3aa68757ed5
Raphael Karanja <rkaranja@cytonn.com>
2019-01-09 10:14:23 +0300

    UPDATE: add the various store class methods; emitChange, addChangeListener, removeChangeListener

--- src/Flux/stores/Store/index.js
+++ src/Flux/stores/Store/index.js
@@ -9,4 +9,15 @@ class Store extends EventEmitter {
     constructor() {
         super();
     }
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    }
+
+    addChangeListener(callback) {
+        this.on(CHANGE_EVENT, callback);
+    }
+
+    removeChangeListener(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    }
 }
