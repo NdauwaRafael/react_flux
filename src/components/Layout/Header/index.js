a4037e7de6b39a76e8f550078edbaea386d4004a
Raphael Karanja <rkaranja@cytonn.com>
2019-01-11 11:39:56 +0300

    UPDATE: Provide text description to Homepage head section

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -6,7 +6,8 @@ import React from 'react';
 export default ()=>
     <header className="header_section">
             <div className="header_section__content">
-                <h3>Home Page Section</h3>
+                <h3>The Urban Repo for Authors and their work!</h3>
+                <button type="button" className="btn btn-info">View More</button>
             </div>
     </header>
 
