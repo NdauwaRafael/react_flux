34e1c8bcf117952818f891fa4aed9dcbd4dab38e
Raphael Karanja <rkaranja@cytonn.com>
2019-01-12 12:54:09 +0300

    UPDATE: Display categories titles

--- src/components/Home/Categories/Category/index.js
+++ src/components/Home/Categories/Category/index.js
@@ -6,6 +6,8 @@ export default props=>
     <div className="category">
         <img src={props.image} alt=""/>
         <div className="category__body">
-
+            <h3>
+                {props.title}
+            </h3>
         </div>
     </div>
