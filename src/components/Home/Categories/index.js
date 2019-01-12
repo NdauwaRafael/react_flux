f66eb361242f49c8af77f87eb7d2e75d0f2d1675
Raphael Karanja <rkaranja@cytonn.com>
2019-01-12 12:44:46 +0300

    UPDATE: Add category titles and pass them as props

--- src/components/Home/Categories/index.js
+++ src/components/Home/Categories/index.js
@@ -6,9 +6,9 @@ import Category from './Category';
 import '../../../css/categories.css';
 export default (props)=>
     <div className="categories">
-        <Category image="/img/img1.jpg" />
-        <Category image="/img/img2.jpg" />
-        <Category image="/img/img3.jpg" />
-        <Category image="/img/img4.jpg" />
-        <Category image="/img/img5.jpg" />
+        <Category image="/img/img1.jpg" title="Historic Discoveries" />
+        <Category image="/img/img2.jpg" title="Social Life" />
+        <Category image="/img/img3.jpg" title="People" />
+        <Category image="/img/img4.jpg" title="Skills and Mastery" />
+        <Category image="/img/img5.jpg" title="Life of Adventure" />
     </div>
