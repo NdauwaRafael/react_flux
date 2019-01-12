4ccb99fae7d0ac7bb480cb33577d782b409c570b
Raphael Karanja <rkaranja@cytonn.com>
2019-01-12 11:50:08 +0300

    UPDATE/; import the categories component in the head page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Header from "../Layout/Header";
+import Categories from './Categories'
 
 class HomePage extends Component {
     render() {
         return (
             <div className="homepage__content">
                 <Header/>
+                <Categories />
             </div>
         );
     }
