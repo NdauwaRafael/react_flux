77003387c2b25a0cbbb6cdaf1d0b36c66d71d0f8
Raphael Karanja <rkaranja@cytonn.com>
2019-01-12 11:42:01 +0300

    UPDATE: Create a Category component as a child of Categories and import it in Categories page

--- src/components/Home/Categories/index.js
+++ src/components/Home/Categories/index.js
@@ -2,7 +2,8 @@
  * Created by Raphael Karanja on 2019-01-12.
  */
 import React from 'react';
+import Category from './Category'
 export default (props)=>
     <div className="categories">
-
+        <Category />
     </div>
