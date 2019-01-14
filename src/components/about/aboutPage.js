0fc744c4013c04c0f4522c86fe763ea7ce1d07f7
Raphael Karanja <rkaranja@cytonn.com>
2019-01-14 20:25:10 +0300

    UPDATE: Add styles for the about page

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -6,17 +6,19 @@ import React, {Component} from 'react';
 class About extends Component {
     render() {
         return (
-            <div>
-                <h1>About </h1>
-                <ul>
-                    <li>React</li>
-                    <li>React Router</li>
-                    <li>Flux</li>
-                    <li>Node</li>
-                    <li>Gulp</li>
-                    <li>Browserify</li>
-                    <li>Bootstrap</li>
-                </ul>
+            <div className="about_page">
+                <div className="about_page__content">
+                    <h1>About </h1>
+                    <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Node</li>
+                        <li>Gulp</li>
+                        <li>Browserify</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </div>
             </div>
         )
     }
