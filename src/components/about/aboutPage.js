db444746c5fe2b8f2a2ae4d97ec3742c39eca55e
Raphael Karanja <rkaranja@cytonn.com>
2019-01-14 21:08:21 +0300

    UPDATE: Add html boilerplate in about page

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -8,16 +8,29 @@ class About extends Component {
         return (
             <div className="about_page">
                 <div className="about_page__content">
-                    <h1>About </h1>
-                    <ul>
-                        <li>React</li>
-                        <li>React Router</li>
-                        <li>Flux</li>
-                        <li>Node</li>
-                        <li>Gulp</li>
-                        <li>Browserify</li>
-                        <li>Bootstrap</li>
-                    </ul>
+                    <div className="header">
+
+                    </div>
+                    <div className="content">
+                        <h1>About </h1>
+                        <p>
+                            The Communications Lead will be tasked with the development of
+                            social media posts, blogs, press releases, video content and other communications
+                            material in line with the communications strategy. Content shall (among others)
+                            document learnings from Pangea Accelerator programs, showcase startups
+                            participating in Pangea Accelerator programs, provide insights into the African startup
+                            ecosystem and share opportunities
+                        </p>
+                    </div>
+                    {/*<ul>*/}
+                    {/*<li>React</li>*/}
+                    {/*<li>React Router</li>*/}
+                    {/*<li>Flux</li>*/}
+                    {/*<li>Node</li>*/}
+                    {/*<li>Gulp</li>*/}
+                    {/*<li>Browserify</li>*/}
+                    {/*<li>Bootstrap</li>*/}
+                    {/*</ul>*/}
                 </div>
             </div>
         )
